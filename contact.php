80641043869fa2e902297cb658065bec23d53c46
AROMA GUPTA <aroma.dev@codeoffun.com>
2022-06-05 17:26:46 +0530

    ss

--- contact.php
+++ contact.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title> Contact Form </title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body> 
-	<div class="container">
+
 		<?php 
 	if(isset($_POST['submit_form']))
 			{ 
@@ -40,23 +31,4 @@ $headers .= "Return-Path: <".$FromEmail.">n";
 } 
 			}
 		?>
-		<?php if(!isset($hide)) { ?>
-			<h2>Contact Us &#9971;</h2>
-	<form action="" method="POST">
-		<label> Name :-</label>
-		<input type="text" name="name" class="form-control" required>
-
-		<label>Email :-</label>
-		<input type="email" name="email" class="form-control" required>
-
-		<label>Phone Number :-</label>
-		<input type="number" name="phone" class="form-control" required>
-
-		<label> Message :-</label>
-		<textarea name="msg" cols="10" rows="5"  class="form-control" required></textarea> 
-		<input type="submit" name="submit_form" value="Send" class="btn-primary">
-</form>
-<?php }?> 
-</div>
-</body>
-</html>
+	
